b5865af9de6c193af8e529686ac2faeb43ab0eb0
Ibrahim Quaizar <48260684+ibbydiggy250@users.noreply.github.com>
2024-08-19 12:52:24 -0400

    Update proph.py

--- proph.py
+++ proph.py
@@ -17,7 +17,13 @@ st.set_page_config('Prophet Model')
 st.title('Predictions for number of items picked by the IDC using the Facebook Prophet Model')
 st.write('Ibrahim Quaizar')
 st.write('August 13th, 2024')
-st.header('Context')
+st.header('Abstract')
+st.write('''Pick Lines are used by the IDC to know how many items must be picked from the shelves on a daily
+         basis. Knowing how many picks they may have to do beforehand can be beneficial to them and their
+         resources. We can use a Facebook prophet model in order to forecast this data for them. With a
+         rough accuracy of 94.8%, and compensations coming into mind, this model can prepare the IDC 2 years
+         in advance, allowing them to more effictively allocate items and workers.''')
+st.header('Brief summary')
 st.write('''Pick lines are the amount of items that the IDC(A storage facility) has to pick from the shelves each day. This may be to ship out to sales, or for whatever other reason.
             The dataset being manipulated is one that shows the number of pick lines on a daily basis. The goal of this project is to try to predict how many picks the IDC might have to do in the
             future so that they are more readily able to allocate workers and items. Based on research, the Facebook Prophet model may be the best to do this because it forecasts data working with
@@ -173,4 +179,3 @@ plt.title('Future Data Points')
 plt.xlabel('Dates')
 plt.ylabel('Pick Lines')
 st.pyplot(fig3)
-
