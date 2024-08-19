0ec5f1e4d2ad7765bc045bea1971e63de04715e9
Ibrahim Quaizar <48260684+ibbydiggy250@users.noreply.github.com>
2024-08-19 10:50:46 -0400

    Update proph.py

--- proph.py
+++ proph.py
@@ -5,21 +5,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from sklearn.metrics import accuracy_score
+from sklearn import metrics
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 def mean_absolute_percentage_error(y_true,y_pred):
     y_true,y_pred = np.array(y_true), np.array(y_pred)
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 pd.set_option('display.max_columns',20)
 df = pd.read_excel('count_model.xlsx', index_col=[0], parse_dates=[0])
 st.set_page_config('Prophet Model')
-st.title('Predictions for number of items picked by the IDC using the Facebook Prophet Model')
+st.title('Predictions for number of items vended using the Facebook Prophet Model')
 st.write('Ibrahim Quaizar')
 st.write('August 13th, 2024')
-st.header('Brief summary')
-st.write('''Pick lines are the amount of items that the IDC(A storage facility) has to pick from the shelves each day. This may be to ship out to sales, or for whatever other reason.
-            The dataset being manipulated is one that shows the number of pick lines on a daily basis. The goal of this project is to try to predict how many picks the IDC might have to do in the
-            future so that they are more readily able to allocate workers and items. Based on research, the Facebook Prophet model may be the best to do this because it forecasts data working with
-            time very well.''')
          
 #print(df.head())
 #Graphing just to see
@@ -34,16 +30,30 @@ train_data, test_data = df[0:train_size], df[train_size:len(df)]
 
 train_data_proph = train_data.reset_index().rename(columns={'demand_date':'ds','pick_lines':'y'})
 test_data_proph = test_data.reset_index().rename(columns={'demand_date':'ds','pick_lines':'y'})
-print(train_data_proph.head())
-print(test_data_proph.head())
+
+
+#print(train_data_proph.head())
+#print(test_data_proph.head())
  
 mod = Prophet()
 mod.add_country_holidays(country_name= 'US')
 mod.fit(train_data_proph)
 y_pred = mod.predict(test_data_proph)
+new_pred = y_pred[['ds','yhat']]
+new_pred = new_pred.rename(columns={'yhat':'y'})
+#pd.to_datetime(test_data_proph['ds'])
+#pd.to_datetime(new_pred['ds'])
+x = test_data_proph['ds'].values.astype('float64')
+y = new_pred['ds'].values.astype('float64')
+new_pred['ds'] = y
+test_data_proph['ds'] = x
+#print(new_pred.dtypes)
+#print(test_data_proph.dtypes)
+accuracy = r2_score(test_data_proph,new_pred)
 
 
-print(y_pred.head())
+test_data_proph['ds'] = pd.to_datetime(test_data_proph['ds'])
+#print(y_pred.head())
 revTable = y_pred[['ds','yhat','yhat_lower','yhat_upper']]
 revTable = pd.merge(test_data_proph,revTable, on = 'ds')
 
@@ -59,27 +69,28 @@ for index, row in revTable.iterrows():
     elif row['actual_pick_lines'] < row['lower_bound']:
         rangee.append('under_range')
 revTable['Range_Status'] = rangee
+#print(revTable.head())
 
 fig, ax = plt.subplots(figsize = (10,5))
 fig1 = mod.plot(y_pred,ax=ax, xlabel = 'Date',ylabel = 'Pick Lines')
 plt.title(label = 'Training & Testing Data')
-plt.show()
+#plt.show()
 fig2 = mod.plot_components(y_pred)
-plt.show()
+#plt.show()
 st.header('Table visualization')
-st.write('''Displayed below is the table showing predicted and actual pick lines from October 2023 to July 2024, along with a lower and upper bound. The Last column
-         shows an analysis if the actual pick lines are within the upper and lower bounds or not. This data was purely a testing dataset, helping to 
-         see how the model actually performed in relation with our actual data. This testing data helps with analyzing how our model functioned, or some 
-         workarounds we may need to take into consideration when trusting these predictions.''')
+st.write('''Table showing predicted and actual pick lines, along with a lower and upper bound. Last column
+         shows an analysis if the actual pick lines are within the upper and lower bounds or not. This helps
+         with figuring out how our model worked, or some workarounds we may need to take into consideration
+         when trusting these predictions.''')
 st.dataframe(revTable)
 
 st.header('Model Visualization through graphs')
-st.write('''Graph showing testing and training data done with the model to achieve prediction results shown below. 
-         upper and lower bound ranges shown. As indicated, the model does predict the training data
-         well, with a few outliers stemming from unforseen events.''')
+st.write('''Graph showing testing and training data done with the model to achieve prediction results. 
+         Upper and Lower bound shades shown. As indicated, the model does predict the training data
+         well, with a few outliers stemming from unforseen events. When checking the accuracy using an 
+         accuracy model, we got an accuracy score of 94.8%, showing a strong prediction set.''')
 st.pyplot(fig1)
-
-st.write('''Components of the above graph, showing the overall trend, weekly tend, and trends near the holidays shown below.
+st.write('''Components of the above graph, showing the overall trend, weekly tend, and trends near the holidays.
          From the overall trend, we can gather that the model is fitting to an upwards trend, which becomes
          important when predicting future years. As for the days of the week, We can see that no production
          happens on weekends, and that production tends to be highest on Thursdays.''')
@@ -91,9 +102,9 @@ plt.xlim(pd.to_datetime('2023-10-21'),pd.to_datetime('2024-07-28'))
 plt.title('Testing Data Predictions')
 plt.xlabel('Dates')
 plt.ylabel('Pick Lines')
-plt.show()
+#plt.show()
 
-st.write('''Below is the zoomed in graph to show just the predicted quantities. Date Range: 10/21/2023 - 07/28/2024.
+st.write('''Zoomed in graph to show just the predicted quantities. Date Range: 10/21/2023 - 07/28/2024.
          Here, we can see a more concrete trendline of the model fitting, and see how many of the predictions
          were accurate. The red dots represent the actual quantities while the blue line represents the
          predicted quantities.''')
@@ -115,14 +126,14 @@ st.pyplot(plt)
 st.header('Future Predictions')
 future = mod.make_future_dataframe(periods = 800, freq = 'd', include_history= False)
 future_fcst = mod.predict(future)
-print(future_fcst.tail())
+#print(future_fcst.tail())
 
 fig, ax = plt.subplots(figsize = (10,5))
 fig = mod.plot(future_fcst, ax=ax)
 plt.title('Future Predictions')
 plt.xlabel('Date')
 plt.ylabel('Pick Lines')
-plt.show()
+#plt.show()
 st.write('''Predictions for the dataset until the end of 2025 shown below. Here we can see that the prophet model 
          fitted to an upward curve shown from the years 2021-2023. This made it so that the amount of items 
          predicted would increase. On a company level, this means that in the future, more items may be needed
